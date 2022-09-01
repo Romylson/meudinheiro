612265c1577bb8642530722305b70a933e2f3f27
Romylson <romylson98@gmail.com>
2022-08-31 21:32:46 -0300

    Tela de Categorias

--- apps/geral/views.py
+++ apps/geral/views.py
@@ -1,9 +1,28 @@
-from django.shortcuts import render
-from django.shortcuts import HttpResponse
-
+from django.shortcuts import render, redirect
+from .forms import CategoriaForm
+from django.contrib import messages
 # Create your views here.
 
 
 def principal(request):
     template_name = 'base.html'
     return render(request, template_name, {})
+
+def nova_categoria(request):
+    template_name = 'geral/nova_categoria.html'
+    context = {}
+    if request.method == 'POST':
+        form = CategoriaForm(request.POST)
+        if form.is_valid():
+            f = form.save(commit=False)
+            f.usuario = request.user
+            f.save()
+            messages.sucess(request, 'Categoria adicionada com sucesso.')
+            return redirect('geral:lista_categorias')
+        else:
+            form = CategoriaForm(request.POST)
+            context['form'] = form
+    else:
+        form = CategoriaForm()
+    context['form'] = form
+    return render(request, template_name, context)
